f668807ef8d47d6b2eaaba7913f267cd7a60ac67
Emil Noren <stratosfloor@hotmail.com>
2023-07-21 16:53:54 +0200

    Added ontap filtering meals

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -3,16 +3,22 @@ import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
+import '../models/category.dart';
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selecCategory(BuildContext context) {
+  void _selecCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals
+        .where((meal) => meal.categories.contains(category.id))
+        .toList();
+
     Navigator.push(
       context,
       MaterialPageRoute(
         builder: (ctx) => MealsScreen(
-          title: 'some title',
-          meals: [],
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     );
@@ -37,7 +43,10 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selecCategory(context);
+                _selecCategory(
+                  context,
+                  category,
+                );
               },
             )
         ],
