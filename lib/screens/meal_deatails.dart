c484b67c2c8530eb7d5c85aac5d926e66beb1bdb
Emil Noren <stratosfloor@hotmail.com>
2023-07-22 18:10:12 +0200

    Added recipie to meal_details screen

--- lib/screens/meal_deatails.dart
+++ lib/screens/meal_deatails.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/widgets/meal_step.dart';
 
 class MealDetailsScreen extends StatelessWidget {
   const MealDetailsScreen({
@@ -15,14 +16,43 @@ class MealDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Column(children: [
-        Image(
-          image: NetworkImage(meal.imageUrl),
-          height: 300,
-          width: double.infinity,
-          fit: BoxFit.cover,
-        ),
-      ]),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        child: ListView(children: [
+          Image(
+            image: NetworkImage(meal.imageUrl),
+            height: 300,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(height: 14),
+          Text(
+            'Ingredients',
+            style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const SizedBox(height: 14),
+          for (final ingredient in meal.ingredients)
+            Text(
+              ingredient,
+              style: Theme.of(context)
+                  .textTheme
+                  .bodyLarge!
+                  .copyWith(color: Theme.of(context).colorScheme.onBackground),
+            ),
+          const SizedBox(height: 24),
+          Text('Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                    fontWeight: FontWeight.bold,
+                  )),
+          ...meal.steps.asMap().entries.map((step) {
+            return MealStep(i: step.key.toString(), text: step.value);
+          }),
+        ]),
+      ),
     );
   }
 }
