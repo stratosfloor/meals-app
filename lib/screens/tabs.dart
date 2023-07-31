95463ef3ce4d54da0745b55f52b74dda39ef7e17
Emil Noren <stratosfloor@hotmail.com>
2023-07-31 13:59:02 +0200

    Added filter screen

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/categories.dart';
+import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 
@@ -25,9 +26,13 @@ class _TabsscreenState extends State<TabsSCreen> {
   }
 
   void _setScreen(String identifier) {
+    Navigator.of(context).pop();
     if (identifier == 'filters') {
-    } else {
-      Navigator.of(context).pop();
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (ctx) => const FiltersScreen(),
+        ),
+      );
     }
   }
 
