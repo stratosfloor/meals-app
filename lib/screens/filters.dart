68ae0fc5e1d3182aa177bfbc05538c44325d8838
Emil Noren <stratosfloor@hotmail.com>
2023-07-31 15:14:21 +0200

    Implemented working filters

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/widgets/filter_item.dart';
 
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
 class FiltersScreen extends StatefulWidget {
-  const FiltersScreen({super.key});
+  const FiltersScreen({
+    super.key,
+    required this.currectFilters,
+  });
+
+  final Map<Filter, bool> currectFilters;
 
   @override
   State<FiltersScreen> createState() {
@@ -17,50 +29,70 @@ class _FiltersScreenState extends State<FiltersScreen> {
   bool _veganFilterSet = false;
 
   @override
+  void initState() {
+    super.initState();
+    _glutenFreeFilterSet = widget.currectFilters[Filter.glutenFree]!;
+    _lactoseFreeFilterSet = widget.currectFilters[Filter.lactoseFree]!;
+    _vegetarianFilterSet = widget.currectFilters[Filter.vegetarian]!;
+    _veganFilterSet = widget.currectFilters[Filter.vegan]!;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Filters'),
       ),
-      body: Column(
-        children: [
-          FilterItem(
-            title: 'Gluten-free',
-            value: _glutenFreeFilterSet,
-            toggle: () {
-              setState(() {
-                _glutenFreeFilterSet = !_glutenFreeFilterSet;
-              });
-            },
-          ),
-          FilterItem(
-            title: 'Lacto-free',
-            value: _lactoseFreeFilterSet,
-            toggle: () {
-              setState(() {
-                _lactoseFreeFilterSet = !_lactoseFreeFilterSet;
-              });
-            },
-          ),
-          FilterItem(
-            title: 'Vegetarian',
-            value: _vegetarianFilterSet,
-            toggle: () {
-              setState(() {
-                _vegetarianFilterSet = !_vegetarianFilterSet;
-              });
-            },
-          ),
-          FilterItem(
-            title: 'Vegan',
-            value: _veganFilterSet,
-            toggle: () {
-              setState(() {
-                _veganFilterSet = !_veganFilterSet;
-              });
-            },
-          ),
-        ],
+      body: WillPopScope(
+        onWillPop: () async {
+          Navigator.of(context).pop({
+            Filter.glutenFree: _glutenFreeFilterSet,
+            Filter.lactoseFree: _lactoseFreeFilterSet,
+            Filter.vegetarian: _vegetarianFilterSet,
+            Filter.vegan: _veganFilterSet,
+          });
+          return false;
+        },
+        child: Column(
+          children: [
+            FilterItem(
+              title: 'Gluten-free',
+              value: _glutenFreeFilterSet,
+              toggle: () {
+                setState(() {
+                  _glutenFreeFilterSet = !_glutenFreeFilterSet;
+                });
+              },
+            ),
+            FilterItem(
+              title: 'Lacto-free',
+              value: _lactoseFreeFilterSet,
+              toggle: () {
+                setState(() {
+                  _lactoseFreeFilterSet = !_lactoseFreeFilterSet;
+                });
+              },
+            ),
+            FilterItem(
+              title: 'Vegetarian',
+              value: _vegetarianFilterSet,
+              toggle: () {
+                setState(() {
+                  _vegetarianFilterSet = !_vegetarianFilterSet;
+                });
+              },
+            ),
+            FilterItem(
+              title: 'Vegan',
+              value: _veganFilterSet,
+              toggle: () {
+                setState(() {
+                  _veganFilterSet = !_veganFilterSet;
+                });
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
 import 'package:meals_app/screens/categories.dart';
 import 'package:meals_app/screens/filters.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/main_drawer.dart';
 
 import '../models/meal.dart';
+
+const kInitialFilters = {
+  Filter.glutenFree: false,
+  Filter.lactoseFree: false,
+  Filter.vegetarian: false,
+  Filter.vegan: false,
+};
 
 class TabsSCreen extends StatefulWidget {
   const TabsSCreen({super.key});
@@ -18,6 +26,7 @@ class TabsSCreen extends StatefulWidget {
 class _TabsscreenState extends State<TabsSCreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoritesMeals = [];
+  Map<Filter, bool> _selectedFilters = kInitialFilters;
 
   void _selectPage(int index) {
     setState(() {
@@ -25,14 +34,20 @@ class _TabsscreenState extends State<TabsSCreen> {
     });
   }
 
-  void _setScreen(String identifier) {
+  void _setScreen(String identifier) async {
     Navigator.of(context).pop();
     if (identifier == 'filters') {
-      Navigator.of(context).push(
+      final result = await Navigator.of(context).push<Map<Filter, bool>>(
         MaterialPageRoute(
-          builder: (ctx) => const FiltersScreen(),
+          builder: (ctx) => FiltersScreen(
+            currectFilters: _selectedFilters,
+          ),
         ),
       );
+
+      setState(() {
+        _selectedFilters = result ?? kInitialFilters;
+      });
     }
   }
 
@@ -61,8 +76,25 @@ class _TabsscreenState extends State<TabsSCreen> {
 
   @override
   Widget build(BuildContext context) {
+    final availableMeals = dummyMeals.where((meal) {
+      if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.lactoseFree]! && !meal.isLactoseFree) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegetarian]! && !meal.isVegetarian) {
+        return false;
+      }
+      if (_selectedFilters[Filter.vegan]! && !meal.isVegan) {
+        return false;
+      }
+      return true;
+    }).toList();
+
     Widget activePage = CategoriesScreen(
       onToggleFavorite: _toggleMealFavorite,
+      availableMeals: availableMeals,
     );
     var activePageTitle = 'Categories';
 
